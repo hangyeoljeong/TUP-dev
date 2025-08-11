b968170cf3f09176ddf75c3e977011fd85d1ab31
wjdghksdl <145414049+wjdghksdl@users.noreply.github.com>
2025-08-11 22:15:57 +0900

    Delete TeamMatching2 directory

--- TeamMatching2/wsgi.py
+++
@@ -1,6 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TeamMatching2.settings')  # 실제 프로젝트 폴더명에 맞게 수정
-
-application = get_wsgi_application()
